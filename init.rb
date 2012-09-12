f719b8a95f298c7ef89418aa7566b28191f90def
MAEDA Go <maeda@farend.jp>
2012-09-12 22:42:32 +0900

    Redmine 2.0 compatibility.

--- init.rb
+++ init.rb
@@ -4,8 +4,7 @@ require_dependency 'scrum_alliance/redmine/issue_status_extensions'
 require_dependency 'scrum_alliance/redmine/project_extensions'
 
 # Dependency loading hell. http://www.ruby-forum.com/topic/166578#new
-require 'dispatcher'
-Dispatcher.to_prepare do
+Rails.configuration.to_prepare do
   Project.class_eval { include ScrumAlliance::Redmine::ProjectExtensions }
   IssueStatus.class_eval { include ScrumAlliance::Redmine::IssueStatusExtensions }
 end
@@ -16,10 +15,10 @@ Redmine::Plugin.register :redmine_task_board do
   description "Creates a drag 'n' drop task board of the items in the current version and their status"
   version '1.1.0-daipresents'
 
-  project_module :task_boards do  
+  project_module :task_boards do
     permission :view_task_boards, :task_boards => :show
     permission :update_task_boards, :task_boards => :update_status
   end
-  
+
   menu :project_menu, :task_board, {:controller => 'task_boards', :action => 'show'}, :before => :burndown, :caption => 'Task Board'
 end
